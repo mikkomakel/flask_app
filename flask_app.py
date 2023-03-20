5ba00c4b0e4a4b836e7afaee9ba28814bb70ee0b
mikkomakel <mikko.makela@seamk.fi>
2023-03-20 10:47:41 +0200

    päivät lisätty

--- flask_app.py
+++ flask_app.py
@@ -8,7 +8,7 @@ lampotilat = [
     {'x': 2, 'y': 10},
     {'x': 3, 'y': 22}
 ]
-paivat = ['Maanantai', 'Tiistai', 'Keskiviikko', 'Lauantai', 'Sunnuntai']
+paivat = ['Maanantai', 'Tiistai', 'Keskiviikko','Torstai', 'Perjantai', 'Lauantai', 'Sunnuntai']
 
 @app.route('/api', methods=['GET'])
 def index():
