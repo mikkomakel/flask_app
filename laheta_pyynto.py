673d2fb0a0c998c14923d2dba6b25c74d1a5b83e
mikkomakel <mikko.makela@seamk.fi>
2023-03-27 12:03:22 +0300

    rakennettu viestin lähettäminen ja tietojen kysely

--- laheta_pyynto.py
+++ laheta_pyynto.py
@@ -5,5 +5,9 @@ import requests
 mittaus = {"x": 6,
            "y": 12}
 
-viesti = json.dumps(mittaus)
-vastaus = requests.post('http://localhost:5000/api', data=viesti)
+while(True):
+    mittaus["x"]= int(input("Anna viikonpäivän numero: "))
+    mittaus["y"]= int(input("Anna lämpötila: "))
+    
+    viesti = json.dumps(mittaus)
+    vastaus = requests.post('http://localhost:5000/lisaa', data=viesti)
